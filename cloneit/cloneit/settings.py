2eca0c9ae80230b4e578e1d59b5b96cec810c4be
Aziz Sharif <58848892+draciel58@users.noreply.github.com>
2020-07-11 12:36:14 +0530

    Update settings.py

--- cloneit/cloneit/settings.py
+++ cloneit/cloneit/settings.py
@@ -124,12 +124,13 @@ LOGIN_URL = '/login/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
-
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+
+
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'azizsharif2000@gmail.com'
-EMAIL_HOST_PASSWORD = ''
+EMAIL_HOST_USER = 'dracielraven589@gmail.com'
+EMAIL_HOST_PASSWORD = 'eyedraven'
 EMAIL_USE_TLS = True
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
