3b514873c2ae38e79615c350adc9f03bfb9397ab
Mukul Choudhury <mukulc998@gmail.com>
2020-07-11 11:43:41 +0530

    placeholders added

--- cloneit/cloneapp/form.py
+++ cloneit/cloneapp/form.py
@@ -2,11 +2,11 @@ from django.forms import ModelForm
 from django import forms
 
 class RegisterForm(forms.Form):
-	username = forms.CharField(max_length=100,widget=forms.TextInput(attrs={'class':'form-control user'}))
-	email = forms.EmailField(max_length=100,widget=forms.TextInput(attrs={'class':'form-control mail'}))
-	password = forms.CharField(max_length=30,widget=forms.PasswordInput(attrs={'class':'form-control pass'}))
-	confirm_password = forms.CharField(max_length=30,widget=forms.PasswordInput(attrs={'class':'form-control confpass'}))
+	username = forms.CharField(max_length=100,widget=forms.TextInput(attrs={'class':'form-control user', 'placeholder': 'Your Name Goes Here...'}))
+	email = forms.EmailField(max_length=100,widget=forms.TextInput(attrs={'class':'form-control mail', 'placeholder': 'A Valid Email Address'}))
+	password = forms.CharField(max_length=30,widget=forms.PasswordInput(attrs={'class':'form-control pass', 'placeholder': 'Enter A Strong Password'}))
+	confirm_password = forms.CharField(max_length=30,widget=forms.PasswordInput(attrs={'class':'form-control confpass', 'placeholder': 'Cross-check Your Password'}))
 
 class SigninForm(forms.Form):
-	username = forms.CharField(max_length=100,widget=forms.TextInput(attrs={'class':'form-control user'}))
-	password = forms.CharField(max_length=30,widget=forms.PasswordInput(attrs={'class':'form-control pass'}))
+	username = forms.CharField(max_length=100,widget=forms.TextInput(attrs={'class':'form-control user', 'placeholder': 'Your Name Goes Here...'}))
+	password = forms.CharField(max_length=30,widget=forms.PasswordInput(attrs={'class':'form-control pass', 'placeholder': 'Enter Your Password'}))
