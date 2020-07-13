3323c25a659fc6e642b6f9ae0810ccbcbc09dbc0
Mukul Choudhury <mukulc998@gmail.com>
2020-07-13 17:11:28 +0530

    added updated cloneit

---
+++ cloneit/cloneapp/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+product_choices=(
+	('laptop','laptop'),
+	('mobile','mobile'),
+	('headphone','headphone'),
+	('speakers','speakers'),
+	('keyboard', 'keyboard'),
+	)
+	
+
+class Products(models.Model):
+	product = models.CharField(max_length=20,choices= product_choices,default='laptop')
+	name = models.CharField(max_length=50,blank=True)
+	description = models.TextField(max_length=2000)
+	image = models.ImageField(upload_to='cloneapp/image',blank=True)
+	price = models.DecimalField(max_digits=7,decimal_places=2)
+
+	def __str__(self):
+		return self.name
