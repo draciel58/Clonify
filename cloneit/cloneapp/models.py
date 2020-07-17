df7ed38d2eb5f7a15e48f62f9b49c18681f21ef8
Aziz Sharif <58848892+draciel58@users.noreply.github.com>
2020-07-17 15:42:21 +0530

    Update models.py

--- cloneit/cloneapp/models.py
+++ cloneit/cloneapp/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 product_choices=(
 	('laptop','laptop'),
@@ -19,3 +20,11 @@ class Products(models.Model):
 
 	def __str__(self):
 		return self.name
+
+class Cartitem(models.Model):
+	user = models.ForeignKey(User,on_delete=models.CASCADE)		
+	item = models.CharField(max_length=50,blank=True,null=True)
+	quantity = models.IntegerField(default=1)
+
+	def __str__(self):
+		return f"{self.user} (item)-{self.item} (quantity)-{self.quantity}"		
