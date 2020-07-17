99b2c4f4fa0f3a5af2a58ec528c47cf9959af228
Aziz Sharif <58848892+draciel58@users.noreply.github.com>
2020-07-17 15:42:56 +0530

    Update admin.py

--- cloneit/cloneapp/admin.py
+++ cloneit/cloneapp/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Products
+from .models import Products,Cartitem
 
 # Register your models here.
 admin.site.register(Products)
+admin.site.register(Cartitem)
