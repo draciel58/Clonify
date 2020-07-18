9d451e50e65539eacc243722eaea658f4f227725
Aziz Sharif <58848892+draciel58@users.noreply.github.com>
2020-07-18 19:03:18 +0530

    Update models.py

--- cloneit/cloneapp/models.py
+++ cloneit/cloneapp/models.py
@@ -16,7 +16,7 @@ class Products(models.Model):
 	short_description = models.TextField(max_length=100,blank=True)
 	description = models.TextField(max_length=2000)
 	image = models.ImageField(upload_to='cloneapp/image',blank=True)
-	price = models.DecimalField(max_digits=7,decimal_places=2)
+	price = models.DecimalField(max_digits=18,decimal_places=2)
 
 	def __str__(self):
 		return self.name
@@ -25,6 +25,8 @@ class Cartitem(models.Model):
 	user = models.ForeignKey(User,on_delete=models.CASCADE)		
 	item = models.CharField(max_length=50,blank=True,null=True)
 	quantity = models.IntegerField(default=1)
+	price = models.DecimalField(max_digits=18,decimal_places=2,null=True)
+	total = models.DecimalField(max_digits=18,decimal_places=2,null=True)
 
 	def __str__(self):
 		return f"{self.user} (item)-{self.item} (quantity)-{self.quantity}"		
