fd04faf1344ae0a635b4cfad53458b8bf20b6e47
Aziz Sharif <58848892+draciel58@users.noreply.github.com>
2020-07-20 12:02:43 +0530

    added profile model

--- cloneit/cloneapp/models.py
+++ cloneit/cloneapp/models.py
@@ -29,4 +29,22 @@ class Cartitem(models.Model):
 	total = models.DecimalField(max_digits=18,decimal_places=2,null=True)
 
 	def __str__(self):
-		return f"{self.user} (item)-{self.item} (quantity)-{self.quantity}"		
+		return f"{self.user} (item)-{self.item} (quantity)-{self.quantity}"	
+
+class Userprofile(models.Model):
+	user = models.ForeignKey(User,on_delete=models.CASCADE,null=True,blank=True)	
+	username = models.CharField(max_length=100,blank=True)
+	phone_number = models.BigIntegerField(blank=True,null=True)
+	email = models.EmailField(max_length=100,blank=True,null=True)
+	address = models.TextField(max_length=400,blank=True,null=True)
+	profile_picture = models.ImageField(upload_to='cloneapp/profile_image',default='cloneapp/image/asus-zenbook.jpg',blank=True,null=True)
+    
+	def __str__(self):
+		return self.username
+
+	@property	
+	def get_photo_url(self):	
+		if self.profile_picture and hasattr(self.profile_picture,'url'):
+			return self.profile_picture.url
+		else:
+			return "media/cloneapp/image/s10.jpg"	
