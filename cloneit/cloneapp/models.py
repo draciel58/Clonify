956e5755645c9f6cada5e4cee8ce6ff47e10b0db
Aziz Sharif <58848892+draciel58@users.noreply.github.com>
2020-07-24 21:14:20 +0530

    added discount on products

--- cloneit/cloneapp/models.py
+++ cloneit/cloneapp/models.py
@@ -17,6 +17,8 @@ class Products(models.Model):
 	description = models.TextField(max_length=2000)
 	image = models.ImageField(upload_to='cloneapp/image',blank=True)
 	price = models.DecimalField(max_digits=18,decimal_places=2)
+	discount = models.IntegerField(null=True,blank=True)
+	discounted_price = models.DecimalField(max_digits=18,decimal_places=2,blank=True,null=True)
 
 	def __str__(self):
 		return self.name
